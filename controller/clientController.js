4d05e719acce525b13611133d40ba484b79dc4c0
Alexandru Burghelea <burghelea.alex@gmail.com>
2017-03-17 19:02:43 +0200

    ..

--- controller/clientController.js
+++ controller/clientController.js
@@ -44,32 +44,45 @@
          };
 
 
-
-
-
-
-
-
          this.addInCart = function()
          {
              console.log(this.quantity);
              console.log(this.selectedElement.price);
 
-             var cost = this.quantity * this.selectedElement.price;
+             var qunatityInCaseTheSame = 0;
 
-             var productFromCartClient ={
+
+             for( var i in cart.listProductInCartFromClient)
+                 {
+                     if(cart.listProductInCartFromClient[i].idLocal == this.selectedElement.idLocal)
+                         {
+                             cart.totalPriceOfCart = cart.totalPriceOfCart - (cart.listProductInCartFromClient[i].quantity * this.selectedElement.price);
+
+                             cart.listProductInCartFromClient[i].quantity =
+                                  cart.listProductInCartFromClient[i].quantity+this.quantity;
+
+                             qunatityInCaseTheSame = cart.listProductInCartFromClient[i].quantity;
+
+                             break;
+                         }
+                 }
+             var cost =0;
+             if(qunatityInCaseTheSame>0){
+                cost = qunatityInCaseTheSame * this.selectedElement.price;
+             }
+             else {
+                 cost = this.quantity * this.selectedElement.price;
+                  var productFromCartClient ={
                  "quantity":this.quantity,
                  "idLocal":this.selectedElement.idLocal
-             };
-
+                  };
              cart.listProductInCartFromClient.push(productFromCartClient);
-
+             }
 
              cart.totalPriceOfCart = cart.totalPriceOfCart + cost;
 
 
              self.succesMsg = "Product added in cart";
-
              console.log(cart);
 
          };
